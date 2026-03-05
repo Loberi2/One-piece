b6a8852be71e9a6f7812e0542e71e2f6e0a51310
Loberi2 <robertzpro2580rrl@gmail.com>
2026-03-05 18:36:47 +0100

    Add files via upload

---
+++ One_Piece/Pagina_Principal/Scripts_Principal.js
@@ -0,0 +1,17 @@
+let preview = document.getElementById("imagen_oculta");
+document.querySelectorAll(".lider").forEach(function(link) {
+    link.addEventListener("mouseenter", function() {
+        let imgSrc = link.getAttribute("data-img");
+        if (imgSrc) {
+            preview.innerHTML = `<img src="${imgSrc}">`;
+        preview.style.display = "block";
+    }  
+    });
+    link.addEventListener("mousemove", function(e) {
+        preview.style.left = e.clientX + "px";
+        preview.style.top = e.clientY + "px";
+    });
+      link.addEventListener("mouseleave", function() {
+        preview.style.display = "none";
+}); 
+});
---
+++ Pruebas/Pruebas.js
@@ -0,0 +1,144 @@
+/* Boton para cambiar el color de fondo a azul al hacer click */
+const boton = document.getElementById("boton");
+boton.addEventListener("click", function() {
+    boton.classList.toggle("azul");
+});
+
+// Cambiar boton de color
+function ShowB2() {
+    let boton2 = document.getElementById("boton2");
+    let boton3 = document.getElementById("boton3");
+    boton2.style.display = "none";
+    boton3.style.display = "block";
+}
+// Detectar numero par o impar
+let botonpar = document.getElementById("botonpar");
+let par = document.getElementById("par");
+let impar = document.getElementById("impar");
+
+botonpar.addEventListener("click", function() { 
+    let numero = document.getElementById("numero").value;
+    if (numero == "") {
+        par.classList.add("ocultar");
+        impar.classList.add("ocultar")
+    }   
+    else if (numero % 2 == 0) {
+            par.classList.remove("ocultar");
+            impar.classList.add("ocultar");
+    }
+    else {
+            impar.classList.remove("ocultar");
+            par.classList.add("ocultar");   
+    }
+
+});   
+
+//Detectar numero mayor o menor
+let botonmayor = document.getElementById("botonmayor");
+let mayor = document.getElementById("mayor");
+let menor = document.getElementById("menor");
+
+botonmayor.addEventListener("click", function() {
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+    switch (true) {
+        case numero1 == numero2:
+            mayor.classList.add("ocultar");
+            menor.classList.add("ocultar");
+            break;
+        case numero1 > numero2:
+            mayor.classList.remove("ocultar");
+            menor.classList.add("ocultar");
+            break;
+        case numero1 < numero2:
+            mayor.classList.add("ocultar");
+            menor.classList.remove("ocultar");
+            break;
+    }
+
+} );
+
+//Piedra Papel o Tijeras
+function jugar(jugada) {
+    let opciones = ["piedra", "papel", "tijeras"];
+    let random = Math.floor(Math.random() * 3);
+    let IA = opciones[random];
+    let resultado = "empate"; 
+    if (jugada == IA) {
+        resultado = "empate";
+    }
+    else if ((jugada == "piedra" && IA == "tijeras") || (jugada == "tijeras" && IA == "papel") || (jugada == "papel" && IA == "tijeras"))
+        resultado = "Victoria"
+    else {
+        resultado = "Derrota"
+    }
+    alert("Jugaste: " + jugada + "\nLa IA jugó: " + IA + "\nEl resultado es: " + resultado);
+}
+
+
+//Imagen en enlace
+const preview = document.getElementById("preview-img");
+
+document.querySelectorAll(".hover-img").forEach(link => {
+
+    link.addEventListener("mouseenter", () => {
+        const imgSrc = link.getAttribute("data-img");
+        preview.innerHTML = `<img src="${imgSrc}">`;
+        preview.style.display = "block";
+    });
+
+    link.addEventListener("mousemove", e => {
+        preview.style.left = e.clientX + "px";
+        preview.style.top = e.clientY + "px";
+    });
+
+    link.addEventListener("mouseleave", () => {
+        preview.style.display = "none";
+    });
+
+});
+// CalcularS
+function calcular(idNum1, idNum2, idResultado, operacion) {
+    let num1 = Number(document.getElementById(idNum1).value);
+    let num2 = Number(document.getElementById(idNum2).value);
+    let resultado;
+
+    if (operacion == "sumar") resultado = num1 + num2;
+    else if (operacion == "restar") resultado = num1 - num2;
+    else if (operacion == "multiplicar") resultado = num1 * num2;
+    else if (operacion == "dividir") resultado = num1 / num2;
+
+    document.getElementById(idResultado).innerHTML = "El resultado es " + resultado;
+}
+
+document.getElementById("sumar").addEventListener("click", function(){
+    calcular("num+1", "num+2", "igual+1", "sumar");
+});
+document.getElementById("restar").addEventListener("click", function(){ 
+    calcular("num-1", "num-2", "igual-1", "restar");
+});
+document.getElementById("multiplicar").addEventListener("click", function(){
+    calcular("num*1", "num*2", "igual*1", "multiplicar");
+});
+document.getElementById("dividir").addEventListener("click", function(){
+    calcular("num/1", "num/2", "igual/1", "dividir");
+});
+//Conversor de valores
+    function celsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+    }
+
+    let conversor = document.getElementById("convertirTemp");
+    function CalcularResultado() {
+         let fahrenheit = document.getElementById("temperaturaF").value;
+         let resultado = celsius(fahrenheit);
+         if (resultado % 1 !== 0) {
+             resultado = resultado.toFixed(2);}
+             return resultado
+           
+    };
+    function mostrarResultado() {
+        document.getElementById("temperaturaC").innerHTML = "La temperatura en C es " + CalcularResultado();
+    }
+
+   
